658f5b0708dc82f916ab8e05069b7a6db4cd7931
joi moore <joimoore0@gmail.com>
2018-09-29 18:17:16 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 require_relative '../lib/greeting.rb
 learn spec/02_cli_spec.rb
-#puts "Hi! I'm HAL, what's your name?"
-#gets.strip "Name"
+puts "Hi! I'm HAL, what's your name?"
+gets.strip "Name"
 end
